ddfe822066b69f0c8058c48ced11e7414fc40173
Artem <smirnov.artem.sam@gmail.com>
2021-09-27 15:43:37 +0400

    initial commit

--- src/main/java/ru/haazad/webapp/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.haazad.webapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.haazad.webapp.models.Role;
-
-@Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
-}
